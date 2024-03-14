74013ba76f4d646a71d68dd180071c4308594d9a
ShinjuEris <145529377+ShinjuEris@users.noreply.github.com>
2024-03-14 23:30:02 +0800

    #Change Controller and Model

---
+++ Rock-Paper-Scissors/Rock-Paper-Scissors/Controllers/COptionController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rock_Paper_Scissors.Models;
+
+namespace Rock_Paper_Scissors.Controllers
+{
+    public class COptionController : Controller
+    {
+        public IActionResult Play()
+        {
+            return View();
+        }
+
+
+        public IActionResult PlayerVsComp(int id)
+        {
+            COption playerChoice = new COption() { Option = (Option)id };
+            COption computerChoice = new COption() { Option = (Option)ComputerChoice() };
+            string str;
+
+            str = WinOrLose(playerChoice, computerChoice, true);
+
+            ViewBag.Choices = "Player : " + playerChoice.Option.ToString() + "\nComputer :" + computerChoice.Option.ToString();
+            ViewBag.Message = str;
+            return View("Result");
+        }
+
+        public IActionResult CompVsComp()
+        {
+            COption computer1Choice = new COption() { Option = (Option)ComputerChoice() };
+            COption computer2Choice = new COption() { Option = (Option)ComputerChoice() };
+            string str = WinOrLose(computer1Choice, computer2Choice, false);
+            ViewBag.Choices = "Computer 1 : " + computer1Choice.Option.ToString() + "\nComputer 2 : " + computer2Choice.Option.ToString();
+            ViewBag.Message = str;
+            return View("Result");
+        }
+        
+        private int ComputerChoice()
+        {
+            Random ran = new Random();
+            return ran.Next(0, 3);
+        }
+
+        private string WinOrLose(COption p1, COption p2, bool type)
+        {
+            string player = type ? "Player" : "Computer";
+            string output = "The winner is ";
+            if ((int)p2.Option == (int)(p1.Option + 1) % 3) output += "Computer 2";
+            else if ((int)p1.Option == (int)(p2.Option + 1) % 3) output += $"{player} 1";
+            else output = "It's a draw";
+
+            return output;
+        }
+    }
+}
--- Rock-Paper-Scissors/Rock-Paper-Scissors/Controllers/HomeController.cs
+++ Rock-Paper-Scissors/Rock-Paper-Scissors/Controllers/HomeController.cs
@@ -1,42 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
 using System;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Rock_Paper.Controllers
 {
     public class HomeController : Controller
     {
-        [HttpPost]
-        public JsonResult Play(string choice)
-        {
-            string[] choices = { "rock", "paper", "scissors" };
-            var random = new Random();
-            var serverChoice = choices[random.Next(0, 3)];
-            var result = "Tie";
-
-            if (choice == "rock" && serverChoice == "scissors" ||
-                choice == "paper" && serverChoice == "rock" ||
-                choice == "scissors" && serverChoice == "paper")
-            {
-                result = "You win!";
-            }
-            else if (serverChoice == "rock" && choice == "scissors" ||
-                    serverChoice == "paper" && choice == "rock" ||
-                    serverChoice == "scissors" && choice == "paper")
-            {
-                result = "Server wins!";
-            }
-
-            var jsonResult = new
-            {
-                userChoice = choice,
-                serverChoice = serverChoice,
-                message = result
-            };
-
-            return Json(jsonResult);
-        }
-
+      
         public IActionResult Index()
         {
             return View();
